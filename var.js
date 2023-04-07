31e5052430ff0e9c873b4c12554c5e78f1a546ba
Martin Jason Alfredo <martinjason2212@gmail.com>
2023-04-07 23:11:00 +0700

    learn js var

---
+++ var.js
@@ -0,0 +1,33 @@
+var a;
+var b = 2;
+
+a = 1;
+b = a;
+
+a = 5;
+b = 7;
+var c = "String";
+var d = " SUPER";
+
+var e = c + d;
+
+console.log(e);
+
+c = c + " Nice!";
+
+console.log(c);
+
+var sum = 1 + 2;
+
+console.log(sum);
+
+sum++;
+
+console.log(sum);
+
+var decimal = 0.009;
+
+console.log(decimal);
+
+var trial = decimal / 5;
+console.log(trial);
